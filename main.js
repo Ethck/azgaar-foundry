74fe863b4b8619c8aaf7196d4f927d6766b89225
Ethck <ethck0720@gmail.com>
2020-11-26 12:34:48 -0800

    Added SVG loading and note creation.

--- main.js
+++ main.js
@@ -28,8 +28,8 @@ class LoadAzgaarMap extends FormApplication {
 
   activateListeners(html) {
     super.activateListeners(html);
-    html.find("input:file").change((event) => this.parseMap(event));
-    html.find("button:submit").click((event) => this.importData(event));
+    html.find("#map").change((event) => this.parseMap(event));
+    //html.find("button:submit").click((event) => this.importData(event));
   }
 
   loadMap(event){
@@ -105,109 +105,132 @@ class LoadAzgaarMap extends FormApplication {
       })
   }
 
-  async importData(event){
+  async importData(){
+    return new Promise(async (resolve, reject) => {
 
-    ui.notifications.notify("UAFMGI: Creating Journals for Cultures.")
-    let cultureComp = await Compendium.create({name: "Cultures", label: "Cultures", entity: "JournalEntry"});
-    let cultureData = this.cultures.map((culture) => {
-      if (!(jQuery.isEmptyObject(culture))){
-          let content = `<div>
-            <h3>${culture.name}</h3>
-            <h4>Type: ${culture.type}</h4>
-            <h4>Expansionism: ${culture.expansionism}</h4>
-            <h4>Color: ${culture.color}</h4>
-            <h4>Code: ${culture.code}</h4>
-          </div>
-          `
+      ui.notifications.notify("UAFMGI: Creating Journals for Cultures.")
+      let cultureComp = await Compendium.create({name: "Cultures", label: "Cultures", entity: "JournalEntry"});
+      let cultureData = this.cultures.map((culture) => {
+        if (!(jQuery.isEmptyObject(culture))){
+            let content = `<div>
+              <h3>${culture.name}</h3>
+              <h4>Type: ${culture.type}</h4>
+              <h4>Expansionism: ${culture.expansionism}</h4>
+              <h4>Color: ${culture.color}</h4>
+              <h4>Code: ${culture.code}</h4>
+            </div>
+            `
 
-          if (culture.name){
-            let journal = {
-              name: culture.name,
-              content: content,
-              permission: {default: 4}
-            };
-            return journal;
-          }
-      }
+            if (culture.name){
+              let journal = {
+                name: culture.name,
+                content: content,
+                permission: {default: 4}
+              };
+              return journal;
+            }
+        }
+      });
+
+      await cultureComp.createEntity(cultureData);
+      this.cultureComp = cultureComp;
+
+      ui.notifications.notify("UAFMGI: Creating Journals for Countries.")
+      let countryComp = await Compendium.create({name: "Countries", label: "Countries", entity: "JournalEntry"})
+      let countryData = this.countries.map((country) => {
+        if (!(jQuery.isEmptyObject(country) || (country.name === "Neutrals"))){
+          // TODO: Extrapolate Provinces, add Burgs?, Neighbors, Diplomacy, Campaigns?, Military?
+            let content = `<div>
+              <h3>${country.fullName}</h3>
+              <h4>Type: ${country.type}</h4>
+              <h4>Expansionism: ${country.expansionism}</h4>
+              <h4>Color: ${country.color}</h4>
+              <h4>Culture: ${this.cultures[country.culture].name}</h4>
+              <h4>Urban: ${country.urban}</h4>
+              <h4>Rural: ${country.rural}</h4>
+              <h4># of Burgs: ${country.burgs}</h4>
+              <h4>Area: ${country.area}</h4>
+              <h4>Form: ${country.form}</h4>
+              <h4>Government: ${country.formName}</h4>
+              <h4>Provinces: ${country.provinces}</h4> 
+            </div>
+            `
+
+            if (country.name){
+               let journal = {
+                name: country.name,
+                content: content,
+                permission: {default: 4}
+              };
+
+              return journal;
+           }
+        }
+      });
+
+      countryData.shift(); // Remove first element. This is the "Neutrals" country.
+
+      await countryComp.createEntity(countryData);
+      this.countryComp = countryComp
+
+      ui.notifications.notify("UAFMGI: Creating Journals for Burgs.")
+      let burgComp = await Compendium.create({name: "Burgs", label: "Burgs", entity: "JournalEntry"});
+      let burgData = this.burgs.map((burg) => {
+        if (!(jQuery.isEmptyObject(burg))){
+
+            let content = `<div>
+              <h3>${burg.name}</h3>
+              <h4>State: ${this.countries[burg.state].name}</h4>
+              <h4>Culture: ${this.cultures[burg.culture].name}</h4>
+              <h4>Population: ${burg.population}</h4>
+              <h4>Citadel: ${burg.citadel}</h4>
+              <h4>Capital: ${burg.capital}</h4>
+              <h4>Port: ${burg.port}</h4>
+              <h4>Plaza: ${burg.plaza}</h4>
+              <h4>Walls: ${burg.walls}</h4>
+              <h4>Shanty: ${burg.shanty}</h4>
+              <h4>Temple: ${burg.temple}</h4>
+              <h4>Feature: ${burg.feature}</h4>
+            </div>
+            `
+
+            if (burg.name){
+               return {
+                name: burg.name,
+                content: content,
+                permission: {default: 4}
+              };
+           }
+        }
+      })
+
+      burgData.shift(); // Remove first element. This is the empty burg.
+      await burgComp.createEntity(burgData);
+      this.burgComp = burgComp;
+
+      resolve();
+
+      //console.log(this.retriveJournalByName({name: "Ahadi"}));
     });
+  }
 
-    await cultureComp.createEntity(cultureData);
-    this.cultureComp = cultureComp;
+  async makeScene(svg){
+    return new Promise(async (resolve,reject) => {
+    //Create The Map Scene
+    let sceneData = await Scene.create(
+      {
+        name: "Azgaar Map",
+        width: 1827,
+        height: 978,
+        padding: 0.0,
+        img: svg
+      });
 
-    ui.notifications.notify("UAFMGI: Creating Journals for Countries.")
-    let countryComp = await Compendium.create({name: "Countries", label: "Countries", entity: "JournalEntry"})
-    let countryData = this.countries.map((country) => {
-      if (!(jQuery.isEmptyObject(country) || (country.name === "Neutrals"))){
-        // TODO: Extrapolate Provinces, add Burgs?, Neighbors, Diplomacy, Campaigns?, Military?
-          let content = `<div>
-            <h3>${country.fullName}</h3>
-            <h4>Type: ${country.type}</h4>
-            <h4>Expansionism: ${country.expansionism}</h4>
-            <h4>Color: ${country.color}</h4>
-            <h4>Culture: ${this.cultures[country.culture].name}</h4>
-            <h4>Urban: ${country.urban}</h4>
-            <h4>Rural: ${country.rural}</h4>
-            <h4># of Burgs: ${country.burgs}</h4>
-            <h4>Area: ${country.area}</h4>
-            <h4>Form: ${country.form}</h4>
-            <h4>Government: ${country.formName}</h4>
-            <h4>Provinces: ${country.provinces}</h4> 
-          </div>
-          `
+    await sceneData.activate();
 
-          if (country.name){
-             let journal = {
-              name: country.name,
-              content: content,
-              permission: {default: 4}
-            };
+    resolve(sceneData);
 
-            return journal;
-         }
-      }
-    });
-
-    countryData.shift(); // Remove first element. This is the "Neutrals" country.
-
-    await countryComp.createEntity(countryData);
-    this.countryComp = countryComp
-
-    ui.notifications.notify("UAFMGI: Creating Journals for Burgs.")
-    let burgComp = await Compendium.create({name: "Burgs", label: "Burgs", entity: "JournalEntry"});
-    let burgData = this.burgs.map((burg) => {
-      if (!(jQuery.isEmptyObject(burg))){
-
-          let content = `<div>
-            <h3>${burg.name}</h3>
-            <h4>State: ${this.countries[burg.state].name}</h4>
-            <h4>Culture: ${this.cultures[burg.culture].name}</h4>
-            <h4>Population: ${burg.population}</h4>
-            <h4>Citadel: ${burg.citadel}</h4>
-            <h4>Capital: ${burg.capital}</h4>
-            <h4>Port: ${burg.port}</h4>
-            <h4>Plaza: ${burg.plaza}</h4>
-            <h4>Walls: ${burg.walls}</h4>
-            <h4>Shanty: ${burg.shanty}</h4>
-            <h4>Temple: ${burg.temple}</h4>
-            <h4>Feature: ${burg.feature}</h4>
-          </div>
-          `
-
-          if (burg.name){
-             return {
-              name: burg.name,
-              content: content,
-              permission: {default: 4}
-            };
-         }
-      }
     })
-
-    burgData.shift(); // Remove first element. This is the empty burg.
-    await burgComp.createEntity(burgData);
-    this.burgComp = burgComp;
-
-    //console.log(this.retriveJournalByName({name: "Ahadi"}));
   }
 
   findObject({type = "burg", name = ""}) {
@@ -225,7 +248,7 @@ class LoadAzgaarMap extends FormApplication {
     return searchable.find((elem) => elem.name === name);
   }
 
-  async retriveJournalByName({type = "burg", name=""}) {
+  async retrieveJournalByName({type = "burg", name=""}) {
     let searchable;
     if (type === "burg") {
       searchable = this.burgComp;
@@ -234,7 +257,6 @@ class LoadAzgaarMap extends FormApplication {
     } else if (type === "culture") {
       searchable = this.cultureComp;
     }
-
     let searchList = await searchable.getIndex();
 
     let id = searchList.find((elem) => elem.name === name)._id;
@@ -245,6 +267,31 @@ class LoadAzgaarMap extends FormApplication {
   }
 
   async _updateObject(event, formData) {
+    // Make the scene
+    let svg = this.element.find('[name="svgMap"]').val()
+    let scene = await this.makeScene(svg);
+
+    // import our data
+    await this.importData();
+
+    // Start applying notes (1:1 ratio for now)
+    this.burgs.forEach((burg) => {
+      let journalEntry = this.retrieveJournalByName({name: burg.name});
+      console.log(journalEntry);
+      //Create a MapNote for this journalEntry, adding it to the active, new, scene.
+      Note.create({
+        entryId: journalEntry._id,
+        x: burg.x,
+        y: burg.y,
+        icon: "icons/svg/village.svg",
+        iconSize: 32,
+        iconTint: "#00FF000",
+        text: burg.name,
+        fontSize: 24,
+        textAnchor: CONST.TEXT_ANCHOR_POINTS.CENTER,
+        textColor: "#00FFFF"
+      });
+    })
     return;
   }
 }
